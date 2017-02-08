0e6cedfc735ac8c19c539a27cd34931709e54315
Han Rui <harryandlina2011@gmail.com>
2017-02-08 17:18:08 +0800

    Add files via upload

---
+++ scripts/SVM/AccurateML.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+mainClass=AccurateML.svm.shun.hash.SVMExample
+
+function do_execute(){
+	args=$1
+	while true
+	do
+		run_time=$SECONDS
+
+		spark-submit \
+		--class $mainClass \
+		--master local[12] \
+		--executor-memory 30g \
+		--driver-memory 8g \
+		AccurateML.jar \
+		$args
+
+		run_time=$(($SECONDS - run_time))
+
+		if [ $run_time -gt 0 ]; then
+			break
+		fi
+
+		sleep 1
+	done
+}
+initW=-1
+step="0"
+numFeature=1000
+inerN=1000
+lrN=10
+testPath="rcv1.test"
+dataPath="rcv1.train"
+test100="false"
+wPath="zip.resultLargeTest50.txt"
+minPartN=1
+
+itqbitN=1
+itqitN=20
+itqrN=10
+upBound=100
+splitN="2.0"
+
+maxRatio=0
+ratio=0
+ratioStep=70
+
+while [ $ratio -le $maxRatio ]; 
+do
+    args="$initW $step $numFeature $inerN $lrN $testPath $dataPath $test100 $wPath $minPartN $itqbitN $itqitN $itqrN $upBound $splitN"
+	do_execute "$args"
+	ratio=$((ratio+ratioStep))
+done
+
---
+++ scripts/SVM/Sampling.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+mainClass=AccurateML.svm.shun.v1.SVMExample
+
+function do_execute(){
+	args=$1
+	while true
+	do
+		run_time=$SECONDS
+
+		spark-submit \
+		--class $mainClass \
+		--master local[12] \
+		--executor-memory 30g \
+		--driver-memory 8g \
+		AccurateML.jar \
+		$args
+
+		run_time=$(($SECONDS - run_time))
+
+		if [ $run_time -gt 0 ]; then
+			break
+		fi
+
+		sleep 1
+	done
+}
+initW=-1
+step="0"
+numFeature=1000
+regP=1000
+lrN=10
+testPath="hdfs:/rcv1.test"
+dataPath="hdfs:/rcv1.train"
+test100="true"
+wPath="RandomResult.txt"
+minPartN=1
+
+maxRatio=0
+ratio=0
+ratioStep=1
+
+while [ $ratio -le $maxRatio ]; 
+do
+    args="$initW $step $numFeature $regP $lrN $testPath $dataPath $test100 $wPath $minPartN"
+	do_execute "$args"
+	ratio=$((ratio+ratioStep))
+done
+
