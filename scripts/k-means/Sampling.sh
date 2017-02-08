e2f3b9465277e682fe4d2ff2da0c055e38ed7fb1
Han Rui <harryandlina2011@gmail.com>
2017-02-08 17:26:11 +0800

    Add files via upload

---
+++ scripts/k-means/Sampling.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+mainClass=AccurateML.kmeans.ZFKmeansPart
+
+function do_execute(){
+	args=$1
+	while true
+	do
+		run_time=$SECONDS
+
+		spark-submit \
+		--class $mainClass \
+		--master local[12] \
+		--executor-memory 30g \
+		--driver-memory 8g \
+		AccurateML.jar \
+		$args
+
+		run_time=$(($SECONDS - run_time))
+
+		if [ $run_time -gt 1 ]; then
+			break
+		fi
+
+		sleep 1
+	done
+}
+k=50
+itN=100
+numFeatures=28102
+centerPath="Enron/part-00000"
+dataPath="hdfs:/Enron"
+test100="false"
+doGradient="false"
+isSparse="true"
+minPartN=4
+
+maxRatio=10
+ratio=10
+ratioStep=10
+
+while [ $ratio -le $maxRatio ]; 
+do
+    args="$k $itN $numFeatures $centerPath $dataPath $test100 $doGradient $isSparse $minPartN"
+	do_execute "$args"
+	ratio=$((ratio+ratioStep))
+done
+
